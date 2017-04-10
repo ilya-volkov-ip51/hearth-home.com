6cb0cc1c67add34eee6683e09b89c1b731b9421c
ilya-volkov-ip51 <volkov_ilya_1998@mail.ru>
2017-04-10 15:51:47 +0300

    commit

---
+++ www/scripts/menuScript.js
@@ -0,0 +1,33 @@
+﻿function load() {
+    menuButton = document.getElementsByClassName('searchButton')[0];
+    searchMenu = document.getElementsByClassName('searchMenu')[0];
+    searchMain = document.getElementsByClassName('searchMain')[0];
+    searchMenu.style.left = '-' + searchMain.offsetWidth + 'px';
+    menuButton.addEventListener('click', menuAppear);
+}
+
+function menuAppear() {
+    smoothChangeCoords(-searchMain.offsetWidth, 0)
+    menuButton.removeEventListener('click', menuAppear);
+    menuButton.addEventListener('click', menuDisappear);
+}
+
+function menuDisappear() {
+    smoothChangeCoords(0, -searchMain.offsetWidth);
+    menuButton.removeEventListener('click', menuDisappear);
+    menuButton.addEventListener('click', menuAppear);
+}
+
+function smoothChangeCoords(start, end) {
+    var step = (end - start) / 25;
+    var moveTimer = setInterval(function () {
+        searchMenu.style.left = start + step + 'px';
+        step += (end - start) / 25;
+        if (Math.abs(step) >= Math.abs((end - start) - (end - start) / 25)) {
+            searchMenu.style.left = end + 'px';
+            clearInterval(moveTimer);
+        }
+    }, 10);
+}
+
+window.addEventListener('load', load);
